4f7c2a9d9b62ee4bd757e3ddb352b6e2e727d7ba
PRERAN S <prerans181@gmail.com>
2026-01-02 00:35:30 +0530

    Fix CORS origin URL for edu-portal-001

--- backeand/server.js
+++ backeand/server.js
@@ -22,7 +22,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://edu-portal-001.vercel.app"
+      "https://edu-portal-001.vercel.app/"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
